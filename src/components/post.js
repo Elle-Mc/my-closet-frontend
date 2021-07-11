46c1b92dfe2e335f4e01769e159d63aa236f08aa
Janelle McGill <you@example.com>
2021-07-11 10:11:10 -0400

    update pages and components

--- src/components/post.js
+++ src/components/post.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Post = (props) => {
-    return <h1>Post</h1>;
+const Post = ({ post }) => {
+    //////////////////
+    // Style Objects
+    //////////////////
+    const div = {
+        textAlign: "center",
+        border: "3px solid",
+        margin: "10px auto",
+        width: "80%"
+    };
+    return (
+        <div style={div}>
+            <Link to={`/post/${post.id}`}>
+            </Link>
+            <h2>{post.details}</h2>
+            <h2>{post.picture}</h2>
+        </div>
+    )
 };
 
 export default Post;
--- src/pages/SinglePost.js
+++ src/pages/SinglePost.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const SinglePost = (props) => {
   return <h1>One Single Post</h1>;
